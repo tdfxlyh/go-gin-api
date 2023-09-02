dae042ba5676cb13b263c4e29870aeeb266024bb
tdfxlyh <1783933591@qq.com>
2023-09-02 21:24:27 +0800

    feat: add Dockerfile and update init db

--- internal/caller/mysql.go
+++ internal/caller/mysql.go
@@ -11,13 +11,19 @@ var (
 	LyhTestDB *gorm.DB
 )
 
-func InitDB() (err error) {
-	dsn := "root:991113@tcp(127.0.0.1:3306)/lyhtest?charset=utf8&parseTime=True&loc=Local"
-	LyhTestDB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		fmt.Println(fmt.Sprintf("database open err, err=%v", err))
-		return
-	}
+func InitDB() {
+	LyhTestDB = InitDBDetail("root", "991113", "lyhtest", "127.0.0.1", 3306)
+	//LyhTestDB = InitDBDetail("root", "991113", "lyhtest", "192.168.23.128", 3306)
+
 	fmt.Println("success...")
 	return
+}
+
+func InitDBDetail(username, password, dbname, ip string, port int64) *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", username, password, ip, port, dbname)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
