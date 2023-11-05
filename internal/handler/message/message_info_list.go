e9ba86badc46f0ea4fca8ccf8fa30cc1fa1cba13
tdfxlyh <1783933591@qq.com>
2023-11-05 13:02:22 +0800

    feat:增加对话列表接口and优化

--- internal/handler/message/message_info_list.go
+++ internal/handler/message/message_info_list.go
@@ -1,1 +1,187 @@
 package message
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/tdfxlyh/go-gin-api/dal/models"
+	"github.com/tdfxlyh/go-gin-api/internal/caller"
+	"github.com/tdfxlyh/go-gin-api/internal/utils"
+	"github.com/tdfxlyh/go-gin-api/internal/utils/output"
+	"github.com/tdfxlyh/go-gin-api/internal/utils/uctx"
+	"sort"
+	"sync"
+)
+
+type GetMessageInfoListHandler struct {
+	Ctx  *gin.Context
+	Resp *GetMessageInfoListResp
+	Lock sync.RWMutex
+
+	FriendUserInfoMap map[int64]*UserItem
+	FriendUserIDs     []int64
+
+	Err error
+}
+
+type GetMessageInfoListResp struct {
+	UserList []*UserItem `json:"msg_list"`
+	Count    int64       `json:"count"`
+}
+
+type UserItem struct {
+	Id        int64  `json:"id"`
+	Name      string `json:"name"`
+	Avatar    string `json:"avatar"`
+	Count     int64  `json:"count"`
+	CountStr  string `json:"count_str"`
+	Timestamp int64  `json:"timestamp"` // 毫秒级别
+	TimeStr   string `json:"time_str"`
+	Desc      string `json:"desc"` // 最新一句话
+}
+
+func NewGetMessageInfoListHandler(ctx *gin.Context) *GetMessageInfoListHandler {
+	return &GetMessageInfoListHandler{
+		Ctx:               ctx,
+		Lock:              sync.RWMutex{},
+		FriendUserIDs:     make([]int64, 0),
+		FriendUserInfoMap: make(map[int64]*UserItem),
+
+		Resp: &GetMessageInfoListResp{
+			UserList: make([]*UserItem, 0),
+		},
+	}
+}
+
+func GetMessageInfoList(ctx *gin.Context) *output.RespStu {
+	h := NewGetMessageInfoListHandler(ctx)
+
+	// 获取好友信息
+	if h.GetFriends(); h.Err != nil {
+		return output.Fail(ctx, output.StatusCodeDBError)
+	}
+
+	// 获取好友用户信息 和消息未读数
+	if h.GetUserInfoAndMsgCount(); h.Err != nil {
+		return output.Fail(ctx, output.StatusCodeDBError)
+	}
+
+	// 打包数据
+	h.PackData()
+
+	return output.Success(ctx, h.Resp)
+}
+
+func (h *GetMessageInfoListHandler) GetFriends() {
+	GetMessageInfoList := make([]models.FriendRelation, 0)
+	h.Err = caller.LyhTestDB.Debug().Table(models.TableNameFriendRelation).Where("status=0 and rela_status=2 and user_id=?", uctx.UID(h.Ctx)).Find(&GetMessageInfoList).Error
+	if h.Err != nil {
+		fmt.Printf("[GetMessageInfoListHandler-GetFriends] db fail, err=%s\n", h.Err)
+		return
+	}
+	for _, friend := range GetMessageInfoList {
+		h.FriendUserIDs = append(h.FriendUserIDs, friend.OtherUserID)
+		h.FriendUserInfoMap[friend.OtherUserID] = &UserItem{
+			Id:   friend.OtherUserID,
+			Name: friend.Notes,
+		}
+	}
+}
+
+func (h *GetMessageInfoListHandler) GetUserInfoAndMsgCount() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		// 获取好友用户信息
+		h.GetUsersInfo()
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		// 获取消息未读数
+		h.GetMsgCount()
+	}()
+	wg.Wait()
+}
+
+func (h *GetMessageInfoListHandler) GetUsersInfo() {
+	userList := make([]models.User, 0)
+	h.Err = caller.LyhTestDB.Debug().Table(models.TableNameUser).Where("uid in (?)", h.FriendUserIDs).Find(&userList).Error
+	if h.Err != nil {
+		fmt.Printf("[GetMessageInfoListHandler-GetFriends] db fail, err=%s\n", h.Err)
+		return
+	}
+	for _, user := range userList {
+		if h.FriendUserInfoMap[user.UID] == nil {
+			continue
+		}
+		if h.FriendUserInfoMap[user.UID].Name == "" {
+			h.Lock.Lock()
+			h.FriendUserInfoMap[user.UID].Name = user.UserName
+			h.Lock.Unlock()
+		}
+		h.Lock.Lock()
+		h.FriendUserInfoMap[user.UID].Avatar = utils.GetPic(user.Avatar)
+		h.Lock.Unlock()
+	}
+}
+
+func (h *GetMessageInfoListHandler) GetMsgCount() {
+	var wg sync.WaitGroup
+	for _, friendID := range h.FriendUserIDs {
+		wg.Add(1)
+		go func(friendID int64) {
+			defer wg.Done()
+			msgList := make([]*models.MessageSingle, 0)
+			caller.LyhTestDB.Table(models.TableNameMessageSingle).
+				Where("(sender_user_id=? and receiver_user_id=?) or (sender_user_id=? and receiver_user_id=?)",
+					friendID, uctx.UID(h.Ctx), uctx.UID(h.Ctx), friendID).
+				Order("create_time desc").Limit(220).
+				Find(&msgList)
+			for _, msg := range msgList {
+				if h.FriendUserInfoMap[friendID].Timestamp == 0 {
+					timeStr := ""
+					if msg.CreateTime.After(utils.GetTodayMidnight()) {
+						timeStr = msg.CreateTime.Format("15:04")
+					} else if msg.CreateTime.After(utils.GetYesterdayMidnight()) {
+						timeStr = msg.CreateTime.Format("昨天")
+					} else if msg.CreateTime.After(utils.GetStartOfYear()) {
+						timeStr = msg.CreateTime.Format("1月2日")
+					} else {
+						timeStr = msg.CreateTime.Format("2006年1月2日")
+					}
+					h.Lock.Lock()
+					h.FriendUserInfoMap[friendID].Timestamp = msg.CreateTime.UnixMilli()
+					h.FriendUserInfoMap[friendID].Desc = msg.Content
+					h.FriendUserInfoMap[friendID].TimeStr = timeStr
+					h.Lock.Unlock()
+				}
+				if msg.ReadStatusInfo == 0 && msg.SenderUserID == friendID {
+					h.Lock.Lock()
+					h.FriendUserInfoMap[friendID].Count++
+					h.Lock.Unlock()
+				}
+			}
+		}(friendID)
+	}
+	wg.Wait()
+}
+
+func (h *GetMessageInfoListHandler) PackData() {
+	for _, friendInfo := range h.FriendUserInfoMap {
+		if friendInfo.Count > 0 && friendInfo.Count <= 99 {
+			friendInfo.CountStr = fmt.Sprintf("%d", friendInfo.Count)
+		} else if friendInfo.Count > 99 {
+			friendInfo.CountStr = "99+"
+		}
+		h.Resp.UserList = append(h.Resp.UserList, friendInfo)
+	}
+	// 按首字符排序
+	sort.Slice(h.Resp.UserList, func(i, j int) bool {
+		if h.Resp.UserList[i].Timestamp == h.Resp.UserList[j].Timestamp {
+			return h.Resp.UserList[i].Id < h.Resp.UserList[j].Id
+		}
+		return h.Resp.UserList[i].Timestamp > h.Resp.UserList[j].Timestamp
+	})
+	h.Resp.Count = int64(len(h.Resp.UserList))
+}
--- internal/handler/user/opt_user.go
+++ internal/handler/user/opt_user.go
@@ -1,1 +1,95 @@
 package user
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/tdfxlyh/go-gin-api/dal/models"
+	"github.com/tdfxlyh/go-gin-api/internal/caller"
+	"github.com/tdfxlyh/go-gin-api/internal/utils"
+	"github.com/tdfxlyh/go-gin-api/internal/utils/output"
+	"github.com/tdfxlyh/go-gin-api/internal/utils/uctx"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type OptUserHandler struct {
+	Ctx *gin.Context
+	Req OptUserReq
+
+	Err error
+}
+
+type OptUserReq struct {
+	OptType  int64  `json:"opt_type"`
+	Avatar   string `json:"avatar"`
+	UserName string `json:"user_name"`
+	Password string `json:"password"`
+}
+
+const (
+	OptAvatar   = 1
+	OptUsername = 2
+	OptPassword = 3
+)
+
+func NewOptUserHandler(ctx *gin.Context) *OptUserHandler {
+	return &OptUserHandler{
+		Ctx: ctx,
+	}
+}
+
+func OptUser(ctx *gin.Context) *output.RespStu {
+	h := NewOptUserHandler(ctx)
+	// 校验参数
+	if msg := h.CheckReq(); msg != "" {
+		return output.FailWithMsg(h.Ctx, output.StatusCodeParamsError, msg)
+	}
+	// 更新数据
+	if h.UpdateUserInfo(); h.Err != nil {
+		fmt.Printf("[OptUser-UpdateUserInfo] err=%s\n", h.Err)
+		return output.Fail(h.Ctx, output.StatusCodeDBError)
+	}
+	return output.Success(h.Ctx, nil)
+}
+
+func (h *OptUserHandler) CheckReq() string {
+	h.Ctx.Bind(&h.Req)
+	switch h.Req.OptType {
+	case OptAvatar:
+	case OptUsername:
+	case OptPassword:
+	default:
+		return "miss opt_type"
+	}
+	if h.Req.OptType == OptAvatar {
+		if h.Req.Avatar == "" {
+			return "miss avatar"
+		}
+		if !utils.CheckOnlinePic(h.Req.Avatar) {
+			return "图片链接不合法"
+		}
+	}
+	if h.Req.OptType == OptUsername && h.Req.UserName == "" {
+		return "miss user_name"
+	}
+	if h.Req.OptType == OptPassword && h.Req.Password == "" {
+		return "miss password"
+	}
+	return ""
+}
+
+func (h *OptUserHandler) UpdateUserInfo() {
+	if h.Req.OptType == OptAvatar {
+		h.Err = caller.LyhTestDB.Table(models.TableNameUser).Where("uid=?", uctx.UID(h.Ctx)).Update("avatar", h.Req.Avatar).Error
+	}
+	if h.Req.OptType == OptUsername {
+		h.Err = caller.LyhTestDB.Table(models.TableNameUser).Where("uid=?", uctx.UID(h.Ctx)).Update("user_name", h.Req.UserName).Error
+	}
+	if h.Req.OptType == OptPassword {
+		hasePassword, err := bcrypt.GenerateFromPassword([]byte(h.Req.Password), bcrypt.DefaultCost)
+		if err != nil {
+			h.Err = err
+			return
+		}
+		h.Err = caller.LyhTestDB.Table(models.TableNameUser).Where("uid=?", uctx.UID(h.Ctx)).Update("password", string(hasePassword)).Error
+	}
+}
