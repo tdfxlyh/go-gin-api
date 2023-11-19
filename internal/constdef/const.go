b1a04e3ee246e9238a27869bf10dcdc38a2dcea0
tdfxlyh <1783933591@qq.com>
2023-11-19 21:19:15 +0800

    feat: update

--- internal/constdef/const.go
+++ internal/constdef/const.go
@@ -8,6 +8,6 @@ const (
 	DBLyhTestUserName = "root"
 	DBLyhTestPassword = "991113"
 	DBLyhTestDBName   = "lyhtest"
-	DBLyhTestIp       = "121.40.249.44" // "127.0.0.1" "192.168.23.128"
+	DBLyhTestIp       = "121.40.249.44" // "127.0.0.1" "192.168.23.128" "121.40.249.44"
 	DBLyhTestPort     = 3306
 )
